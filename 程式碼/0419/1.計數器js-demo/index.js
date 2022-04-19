79badcc5e80d382836c55af63d2ec47a0bc19c6c
yisichoco <yisichoco@gmail.com>
2022-04-19 14:04:43 +0800

    A

---
+++ 程式碼/0419/1.計數器js-demo/index.js
@@ -0,0 +1,4 @@
+document.getElementById("total").addEventListener('click', function(){
+    document.getElementById("total").innerHTML = 
+      Number(document.getElementById("total").innerHTML) + 1
+})
