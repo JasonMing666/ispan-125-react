82aee035a3e1da405d91c25e3ae3d5d5edda098b
yisichoco <yisichoco@gmail.com>
2022-04-22 11:00:25 +0800

    A

---
+++ 程式碼/0422/6.練習題2提示講解/src/App.js
@@ -0,0 +1,11 @@
+import Student from "./homework2/Student"
+
+function App() {
+  return (
+    <>
+      <Student />
+    </>
+  )
+}
+
+export default App
---
+++ 程式碼/0422/6.練習題2提示講解/src/homework2/Student.js
@@ -0,0 +1,14 @@
+//import data from '../data/student.json'
+import { data } from '../data/student'
+
+function Student() {
+  console.log(data)
+
+  return (
+    <>
+      <h1>Student</h1>
+    </>
+  )
+}
+
+export default Student
