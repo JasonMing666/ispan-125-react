2c6527d928b2808b31817b6c034d9ad6564a7376
yisichoco <yisichoco@gmail.com>
2022-04-25 11:50:24 +0800

    Update Controlled.js

--- 程式碼/0425/1.可控表單元素-input-text-textarea/ex-0425/Controlled.js
+++ 程式碼/0425/1.可控表單元素-input-text-textarea/ex-0425/Controlled.js
@@ -6,21 +6,25 @@ function Controlled() {
 
   return (
     <>
-      <h2>文字輸入框</h2>
-      <input
-        type="text"
-        value={inputText}
-        onChange={(e) => {
-          setInputText(e.target.value)
-        }}
-      />
-      <h2>文字輸入區域</h2>
-      <textarea
-        value={textArea}
-        onChange={(e) => {
-          setTextArea(e.target.value)
-        }}
-      />
+      <section id="input-text">
+        <h2>文字輸入框</h2>
+        <input
+          type="text"
+          value={inputText}
+          onChange={(e) => {
+            setInputText(e.target.value)
+          }}
+        />
+      </section>
+      <section id="textarea">
+        <h2>文字輸入區域</h2>
+        <textarea
+          value={textArea}
+          onChange={(e) => {
+            setTextArea(e.target.value)
+          }}
+        />
+      </section>
     </>
   )
 }
