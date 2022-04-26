b88b08645f54524ff3f8e69cf88ea8057227ec39
yisichoco <yisichoco@gmail.com>
2022-04-26 16:02:41 +0800

    A

--- 程式碼/0426/9.生命周期-類別型元件/index.js
+++ 程式碼/0426/9.生命周期-類別型元件/index.js
@@ -5,6 +5,11 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// 注意: React 18中的strict mode會在mounts時作立即unmount又重新mount模擬
+// 只有development模式會這樣，所以會有生命周期方法測試時不匹配的問題
+// 可以先移除 index.js中的<React.StrictMode>元件再測試
+// 參考: https://reactjs.org/docs/strict-mode.html#ensuring-reusable-state
 root.render(
   //<React.StrictMode>
   <App />
