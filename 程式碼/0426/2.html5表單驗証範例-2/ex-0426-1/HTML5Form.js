381beef7c0b22eff2fae0c57f2dacc084c6a843e
yisichoco <yisichoco@gmail.com>
2022-04-26 10:22:45 +0800

    A

---
+++ 程式碼/0426/2.html5表單驗証範例-2/ex-0426-1/HTML5Form.js
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+function HTML5Form() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [birth, setBirth] = useState('')
+
+  return (
+    <>
+      <h1>HTML5表單驗証</h1>
+      <form>
+        <div>
+          <label>帳號</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value)
+            }}
+            required
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+            required
+          />
+        </div>
+        <div>
+          <label>密碼</label>
+          <input
+            type="text"
+            name="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            required
+          />
+        </div>
+        <div>
+          <label>出生年月日</label>
+          <input
+            type="date"
+            name="birth"
+            value={birth}
+            onChange={(e) => {
+              setBirth(e.target.value)
+            }}
+            required
+          />
+        </div>
+        <button type="submit">送出</button>
+      </form>
+    </>
+  )
+}
+
+export default HTML5Form
