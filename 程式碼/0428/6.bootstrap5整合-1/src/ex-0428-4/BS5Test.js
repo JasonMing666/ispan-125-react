64fdfa9e81a81701199ec775a063adb65004fccf
yisichoco <yisichoco@gmail.com>
2022-04-28 14:05:17 +0800

    A

---
+++ 程式碼/0428/6.bootstrap5整合-1/src/ex-0428-4/BS5Test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function BS5Test(props) {
+  return (
+    <div>
+      <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+      <div class="alert alert-secondary" role="alert">
+        A simple secondary alert—check it out!
+      </div>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" class="btn btn-success">
+        Success
+      </button>
+    </div>
+  )
+}
+
+export default BS5Test
