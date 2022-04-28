d7c923741b4b9f591a696af9fc03a198f8596246
yisichoco <yisichoco@gmail.com>
2022-04-28 10:06:38 +0800

    Update UserList.js

--- 程式碼/0428/2.didMount階段-載入資料-1/ex-0428-2/UserList.js
+++ 程式碼/0428/2.didMount階段-載入資料-1/ex-0428-2/UserList.js
@@ -4,14 +4,18 @@ function UserList(props) {
   const [users, setUsers] = useState([])
 
   const fetchUser = async () => {
+    //向遠端伺服器get資料
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await response.json()
+
+    // 載入資料後設定到狀態中
+    // 設定到狀態後，因改變狀態會觸發updating生命周期，然後重新render一次
     setUsers(data)
   }
 
-  // didMount
+  // didMount - 載入資料的時間點
   useEffect(() => {
-    // 載入資料的時間點
+    // 向伺服器要求get資料
     fetchUser()
   }, [])
 
